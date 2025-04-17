278a6c49bf8ddda89828939364ec117b4c478924
arunv369 <2001arunv@gmail.com>
2025-04-17 16:46:10 +0530

    signup/login commit

--- controller/PasswordController.js
+++ controller/PasswordController.js
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.signUp = async (req, res) => {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
@@ -24,10 +24,31 @@ exports.signUp = async (req, res) => {
       return res.status(400).json({ msg: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ email, password: hashedPassword });
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ msg: "User created successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
+// Login
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, user: { name: user.name, email: user.email } });
   } catch (err) {
     res.status(500).json({ msg: "Server error" });
   }
--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,21 @@ const {
   forgotPassword,
   resetPassword,
   signUp,
+  login,
 } = require("../controller/PasswordController");
 
 const router = express.Router();
+
+//signup
+router.post("/signup", signUp);
+
+//login
+router.post("/login", login);
 
 // Forgot Password Route
 router.post("/forgot-password", forgotPassword);
 
 // Reset Password Route
 router.post("/reset-password/:token", resetPassword);
-router.post("/signup", signUp);
 
 module.exports = router;
